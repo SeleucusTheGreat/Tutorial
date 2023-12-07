21c6758287b9b1919f9e3932dd53ff72bd3ca73e
Frank <francasac@yahoo.it>
2023-12-07 19:05:07 +0100

    added a profile for users, added a name for creating a new profile with all the controllers necessary

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/profile'
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
+  get "/u/1", to: "users#profile", as: "user"
   resources :posts
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'

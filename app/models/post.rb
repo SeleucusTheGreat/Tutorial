807bc26a06cb14f43431e18cbc839309194323fe
Frank <francasac@yahoo.it>
2023-12-12 21:11:45 +0100

    I've started to add the admin functionality to the users

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,12 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments , dependent: :destroy
 
+
     has_noticed_notifications model_name: "Notification"
     has_many :notifications, through: :user 
 
     def self.ransackable_attributes(auth_object = nil)
-        ["body", "created_at", "id", "id_value", "title", "updated_at", "user_id", "views"]
+        [ "body", "id", "id_value", "title", "updated_at", "user_id", "views"]
     end
 
     def self.ransackable_associations(auth_object = nil)
